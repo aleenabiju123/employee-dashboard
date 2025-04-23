0c83c36eeeb1804d8798efa80c5a64757e3d5a52
aleenabiju123 <150226690+aleenabiju123@users.noreply.github.com>
2025-04-23 09:28:04 +0530

    Add files via upload

--- src/components/Employee.jsx
+++ src/components/Employee.jsx
@@ -9,17 +9,20 @@ const Employee = () => {
         }
   return (
     <div>
+      <form align="center">
        <Box sx={{padding:4,
             backgroundColor:"white",
             borderRadius:2,
             marginTop:5,
             boxShadow:3}}>
       <Typography variant='h4' gutterBottom>Signup Form</Typography>
-      <TextField label="Fullname" variant="outlined" name='Fname'  onChange={inputHandler}/><br></br><br></br>
-      <TextField label="Email" variant="outlined" name="Email" onChange={inputHandler} /><br></br><br></br>
-      <TextField label="Password" variant="outlined" name="Password" onChange={inputHandler} /><br/><br/>
-      <Button variant="contained">SignUp</Button>
+      <TextField fullWidth label="Name" variant="outlined" name='Fname'  onChange={inputHandler}/><br></br><br></br>
+      <TextField fullWidth label="Designation" variant="outlined" name="dname" onChange={inputHandler} /><br></br><br></br>
+      <TextField fullWidth label="Location" variant="outlined" name="lname" onChange={inputHandler} /><br/><br/>
+      <TextField fullWidth label="Salary" variant="outlined" name="salary" onChange={inputHandler} /><br/><br/>
+      <Button variant="contained">Submit</Button>
       </Box>
+      </form>
     </div>
   )
 }
